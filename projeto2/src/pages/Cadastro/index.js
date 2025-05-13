407df8a12f671eab5c072965f8d615c381dfc2aa
Luis02022 <160199124+Luis02022@users.noreply.github.com>
2025-05-13 11:32:48 -0300

    Add files via upload

---
+++ projeto2/src/pages/Cadastro/index.js
@@ -0,0 +1,23 @@
+// src\pages\Cadastro\index.js
+
+import ListaDeUsuarios from '../components/ListaDeUsuarios';
+import {useNavigate} from 'react-router-dom';
+import './styles.css'
+
+
+function PaginaListaUsuarios() {
+    const navigate = useNavigate()
+
+    return(
+        <div className='pagina-lista-usuarios'>
+            <div className='container'>
+                <h2>Kusta de usuarios</h2>
+                <ListaDeUsuarios/>
+                <button onClick={() => navigate('/')} className='link-voltar'>
+                    Cadastrar usuario
+                </button>
+            </div>
+        </div>
+    )
+}
+export default PaginaListaUsuarios;
